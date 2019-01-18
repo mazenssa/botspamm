897beb8736f5afe0cf7f5152248340fe52e198e6
mazenssa <46714246+mazenssa@users.noreply.github.com>
2019-01-19 01:07:57 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ console.log("Scrpit By Dream");
 client.on("ready", () => {
 let channel =     client.channels.get("534484425300508696")
 setInterval(function() {
-channel.send(`hell gays hello mazen ana esmy mazen ayman mohamed elsayed mazen  hello`);
+channel.send(`hell gays hello mazen ana esmy mazen ayman mohamed elsayed mazen  hello asdasdasdasnfjkahns fsdashkgdakjgf gaaygdaygtdagduagdgsaudguwagdgyuas agduadiwdgasudwaudgasudf dssgsuygdfd audfasudfusffgu dytr geufdgsufgsutfgsduyf suydgusgdusgdfusd sfsfsfdsdfsdfsdfsdf4sfghsdgfjsdgfgsdgfsgyefgsdfgeshflhsfhskfsgkfesfslfsl;/egesgfesgdfgeygsdygfygsfgsdfgesu yfes ye  eysfes f  esyfgseygfgedyyeg eesy seyf sf sfgesygf te 36rte ey4666666 665as58756 585 6586 58 565 8567 65458 556 7865 85 5 85 4 54 8 5 gasdgsjagdsajgdfasfgasjfdajgdagsdas544455555555555  55 5 55 5 5 5 55 5 asudsagdjsagdasgduagjfsdjgffskhg kjdf ghiufkdsg shf sdjk fgdsf hkhdf kjsdkhfkjsd fgsdfkjsdjfgdsjgfjsdfjsfjskfkjafhkgdafhkghdkagakadghkgurthuhfdghkahghfalghailgladghhaldfghuagdadfghl hgfdghijhgfhsd fsdhfysdfsgyyfds fdhsgfjkgjfkj a fdgkjfdgkj afhgkjafdhghjkgdhgkghdkd hf gh SHGH k HGG G FGFGFGFGKDF UISGSFGDFUIGDSI OGSF ISDFSDG FSBIF SIF ISGFSDG`);
 }, 30)
 })
 
